7e49f846bfe922a0f995e50a936f222f0005c1c4
Alexsis24 <53233129+Alexsis24@users.noreply.github.com>
2023-03-09 15:57:05 +0200

    Global using

--- Scientific-Calculator/AppShell.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Scientific_Calculator;
-
-public partial class AppShell : Shell
-{
-	public AppShell()
-	{
-		InitializeComponent();
-	}
-}
---
+++ Scientific-Calculator/Views/CalculatorPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Scientific_Calculator.Views;
+
+public partial class CalculatorPage : ContentPage
+{
+	public CalculatorPage()
+	{
+		InitializeComponent();
+	}
+}
